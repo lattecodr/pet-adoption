82cd5d80b3bc7305df0602cccc0628f434fa3e2b
LatteCodr <166710661+lattecodr@users.noreply.github.com>
2025-12-25 19:29:17 +0700

    Nearly finished pet card feature

--- main.js
+++ main.js
@@ -20,9 +20,26 @@ async function petsArea() {
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
     clone.querySelector("h3").textContent = pet.name;
+    clone.querySelector(".pet-description").textContent = pet.description;
+    clone.querySelector(".pet-age").textContent = calPetAge(pet.birthYear);
+    clone.querySelector(".pet-card-photo img").src = pet.photo;
+    clone.querySelector(
+      ".pet-card-photo img"
+    ).alt = `A ${pet.species} name ${pet.name}`;
+
     wrapper.appendChild(clone);
     document.querySelector(".list-of-pets").appendChild(wrapper);
   });
 }
 
 petsArea();
+
+function calPetAge(birthYear) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+
+  if (age == 0) return "less than a year old";
+  if (age == 1) return "1 year old";
+
+  return `${age} years old`;
+}
